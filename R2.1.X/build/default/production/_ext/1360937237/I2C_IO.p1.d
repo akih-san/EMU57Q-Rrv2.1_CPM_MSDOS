52d9a7277810e56fbb8a661b3cc3077271ac5174
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-06-18 16:15:48 +0900

    Delete R2.1.X directory

--- R2.1.X/build/default/production/_ext/1360937237/I2C_IO.p1.d
+++
@@ -1,6 +0,0 @@
-build/default/production/_ext/1360937237/I2C_IO.p1:  \
-../src/I2C_IO.c  \
-../src/../src/emu88.h  \
-../src/../src/picconfig.h  \
-../src/../fatfs/ff.h  \
-../src/../fatfs/../fatfs/ffconf.h 
--- R2.1.X/build/default/production/_ext/1360937237/emu88_main.p1.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/1360937237/emu88_main.p1:  \
-../src/emu88_main.c  \
-../src/../src/emu88.h  \
-../src/../src/picconfig.h  \
-../src/../fatfs/ff.h  \
-../src/../fatfs/../fatfs/ffconf.h  \
-../src/../drivers/utils.h 
