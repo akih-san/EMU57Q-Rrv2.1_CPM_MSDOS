913bfb90c07bcd647ba9833b304147d32186e5fe
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-06-18 16:16:33 +0900

    Add files via upload

---
+++ R2.1.X/build/default/production/_ext/1360937237/emu88_main.p1.d
@@ -0,0 +1,7 @@
+build/default/production/_ext/1360937237/emu88_main.p1:  \
+../src/emu88_main.c  \
+../src/../src/emu88.h  \
+../src/../src/../src/picconfig.h  \
+../src/../src/../fatfs/ff.h  \
+../src/../src/../fatfs/../fatfs/ffconf.h  \
+../src/../drivers/utils.h 
